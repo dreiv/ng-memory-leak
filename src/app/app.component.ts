3c09fd7ea2f6f1d8b19b2c3bd2b5e1ab1f3037fc
Andrei Voicu <dreivoicu@gmail.com>
2020-06-26 20:49:10 +0300

    simulate usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<app-sub *ngIf="show"></app-sub>',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  show = true;
+
+  ngOnInit(): void {
+    setInterval(
+      () => this.show = !this.show,
+      50
+    );
+  }
 }
